3b361fb58c89373598b3274031cc70e30fcc5d69
VARUN S <varuuun.s@gmail.com>
2025-10-30 17:09:25 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const { default: mongoose } = require("mongoose");
+const { MONGO_URL } = require("../config");
+const app=express();
+
+const connectDB = async () => {
+    await mongoose.connect(MONGO_URL)
+        .then(() => console.log(`mongodb connected`))
+        .catch((err) => console.log(`error from db: ${err}`));
+}
+connectDB();
+ 
+app.post("/signup", (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    
+})
+
+app.use("/",(req,res)=>{
+    res.send("404 wrong page");
+})
+
+app.listen(3000,()=>{
+    console.log(`server listening on 3333`);
+})
+ 
